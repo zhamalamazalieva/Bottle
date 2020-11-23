981ae6532828105dae7de04878ef0e60d47588d6
zhamalamazalieva <jamalamazalieva@gmail.com>
2020-11-23 09:39:55 +0100

    finished the section 'hero'

---
+++ js/index.js
@@ -0,0 +1,13 @@
+    function myFunction(){
+        var moreText = document.getElementById("more");
+        var linkText = document.getElementById("myLink");
+        if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            linkText.innerHTML = "Read more"; 
+            moreText.style.display = "none";
+          } else {
+            dots.style.display = "none";
+            linkText.innerHTML = "Read less"; 
+            moreText.style.display = "inline";
+          }
+    }
