103f016bcf4b419b0c4e1e3def4d6c0bff06625c
mynarbek <cymbatovmunar@gmail.com>
2020-11-26 10:52:57 +0600

    test

--- js/index.js
+++ js/index.js
@@ -13,3 +13,15 @@ function myFunction() {
       moreText.style.display = "inline";
     }
   }
+
+  $('.new__slider').slick({
+   arrows: true,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+  //  autoplay: true,
+  //  autoplaySpeed: 2000,
+   prevArrow:'.slider__prev',
+   nextArrow:'.slider__next'
+
+  });
+            
