29db1feba0d3bcbb66d87a185139ef6589bd2a42
zhamalamazalieva <jamalamazalieva@gmail.com>
2020-11-26 10:40:44 +0100

    added improvments to slider in 'offer'

--- js/index.js
+++ js/index.js
@@ -34,7 +34,8 @@ $('.offer__slider').slick({
   arrows:false,
   dots:true,
   slidesToShow:3,
-  slidesToScroll:1
+  slidesToScroll:1,
+  centerMode: true
 })
      
 /*Function for slider in 'EVENTS'
@@ -45,12 +46,12 @@ $('.events__slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
 });
-        
+
 /*Function for slider FILTER-IS-ACTIVE in 'SORT'
 =============================================*/
 function sortSection() {
   const sortItems = document.querySelectorAll('.filter__items');
-  const sortItemsName = ['All', 'Salad', 'Fast Food', 'Platters', 'Dessert'];
+  const sortItemsName = ['All', 'Red', 'Rose', 'White'];
   sortItems.forEach((item, index) => {
     item.addEventListener('click', () => {
       sortItems.forEach((everyElement) => {
@@ -65,3 +66,39 @@ function sortSection() {
   });
 }
 sortSection();
+function sortFilter(index) {
+  const filterItems = document.querySelectorAll('.sort__item');
+  const filterItemsName = [
+    'data-all',
+    'data-red',
+    'data-rose',
+    'data-white',
+  ];
+  filterItems.forEach((ayday) => {
+    if (!ayday.hasAttribute(filterItemsName[index])) {
+      ayday.style.display = 'none';
+    } else {
+      ayday.style.display = 'block';
+    }
+  });
+}
+
+// function activeFinder(i = 1) {
+//   let activeSliders = document.querySelectorAll('.slick-active.col-4');
+//   activeSliders.forEach(item => {
+//     item.classList.remove('slick-big')
+//   })
+//   activeSliders[i].classList.toggle('slick-big')
+// }
+// activeFinder()
+// const buttons = document.querySelectorAll('.offer button');
+// console.log(buttons)
+// buttons.forEach((item,index) => {
+//   item.addEventListener('click', () => {
+//     let counter = index;
+//     console.log(counter)
+//     buttons[index]
+//     console.log(index)
+//     activeFinder(2)
+//   })
+// })
